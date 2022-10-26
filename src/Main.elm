3882aed6f2492f49d46d4b6fc1772afca3513e39
Evgeny Pestov <pestoffne@vtomske.ru>
2022-10-27 01:39:22 +0700

    Add bubbles list

--- src/Main.elm
+++ src/Main.elm
@@ -12,32 +12,31 @@ import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
-type alias Model =
-    { count : Float }
-
-
 type Msg
     = Tick Float
     | Catch
 
 
-width = 512
+width = 333
 height = 512
 
 
 main =
     Browser.element
-        { init = \() -> ( { tick = 0, scores = 0, bubbles = [] }, Cmd.none )
-        , view = view
-        , update =
-            \msg model ->
-                case msg of
-                    Tick _ ->
-                        ( { model | tick = model.tick + 1 }, Cmd.none )
-                    Catch ->
-                        ( catch model, Cmd.none )
-        , subscriptions = \model -> onAnimationFrameDelta Tick
-        }
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = \model -> onAnimationFrameDelta Tick
+    }
+
+
+init () =
+    ({ tick = 0, scores = 0,
+        bubbles = [
+            { x = 100, y = 256, radius = 30, rotation = 90 },
+            { x = 200, y = 256, radius = 20, rotation = 0 }
+        ]
+    }, Cmd.none)
 
 
 view model =
@@ -46,15 +45,18 @@ view model =
     [ Canvas.toHtml
         ( width, height )
         [ style "border" "10px solid rgba(0,0,0,0.1)", onClick Catch ]
-        [ clearBox, printScores model ]
+        [ clearBox, drawBubbles model.bubbles, printScores model ]
     ]
 
 
+update message model =
+    case message of
+        Tick _ -> ( { model | tick = model.tick + 1 }, Cmd.none )
+        Catch -> ( catch model, Cmd.none )
+
+
 catch model =
-    { model
-        | scores = model.scores + 1
-        , tick = 0
-    }
+    { model | scores = model.scores + 1, tick = 0 }
 
 
 clearBox =
@@ -63,8 +65,18 @@ clearBox =
     [ rect ( 0, 0 ) width height ]
 
 
+drawOneBubble bubble =
+    circle ( bubble.x, bubble.y ) bubble.radius
+
+
+drawBubbles bubbles =
+    Canvas.shapes
+    [ fill Color.blue ]
+    ( List.map drawOneBubble bubbles )
+
+
 printScores model =
     Canvas.text
-    [ font { size = 32, family = "serif" } , align Left ]
-    ( 4, 50 )
+    [ font { size = 20, family = "serif" } , align Left, baseLine Top ]
+    ( 4, 6 )
     ( "Scores = " ++ String.fromInt model.scores ++ ", Tick = " ++ String.fromInt model.tick)
