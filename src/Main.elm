3445fdb843a5984d2933a1ff06e01891b50fcaaf
Evgeny Pestov <pestoffne@vtomske.ru>
2022-10-28 03:38:41 +0700

    Generate bubbles more randomly

--- src/Main.elm
+++ src/Main.elm
@@ -22,24 +22,9 @@ import Random
 type Msg
     = Tick Float
     | StartAt Mouse.Event
-    --| MoveAt Mouse.Event
-    --| EndAt Mouse.Event
-    -- from_x, to_x, radius, velocity
-    --| MintBubble ( Float, Float, Float, Float )
-    | MintBubble Float
-
-
-canvasWidth = 400
-canvasHeight = 512
-maxBubblesCount = 7
-bubbleGenerationRate = 0.05
-bubbleMinSpeed = 2.5
-bubbleMaxSpeed = 5.0
-bubbleMinRadius = 20
-bubbleMaxRadius = 90
-bubbleMinX = 30
-bubbleMaxX = canvasWidth - bubbleMinX
-bubbleGenerator = Random.float bubbleMinX bubbleMaxX
+    | MintBubble
+        { shape: Circle2d.Circle2d Length.Meters Float
+        , velocity: Vector2d.Vector2d Length.Meters Float }
 
 
 main =
@@ -48,26 +33,7 @@ main =
 
 
 init () =
-    ({ tick = 0, scores = 0, bubbles = [
-        -- first bubble
-        { shape =
-            Circle2d.atPoint
-            ( Point2d.fromTuple Length.cssPixels ( 100, 256 ) )
-            ( Length.cssPixels 100 )
-        , velocity =
-            Vector2d.withLength
-            ( Length.cssPixels 2.5 )
-            Direction2d.positiveX },
-        -- second bubble
-        { shape =
-            Circle2d.atPoint
-            ( Point2d.fromTuple Length.cssPixels ( 300, 256 ) )
-            ( Length.cssPixels 30 )
-        , velocity =
-            Vector2d.withLength
-            ( Length.cssPixels 5 )
-            Direction2d.positiveY }
-    ]}, Cmd.none)
+    ({ tick = 0, scores = 0, bubbles = [] }, Cmd.none)
 
 
 view model =
@@ -78,8 +44,6 @@ view model =
         ( canvasWidth, canvasHeight )
         [ style "border" "10px solid rgba(0,0,0,0.1)"
         , Mouse.onDown StartAt ]
-        --, Mouse.onMove MoveAt
-        --, Mouse.onUp EndAt
         [ clearCanvas
         , renderBubbles model.bubbles
         , renderScores model ]
@@ -94,7 +58,7 @@ update message model =
             },
                 if 
                     ( List.length model.bubbles < maxBubblesCount ) &&
-                    ( remainderBy 100 model.tick == 0 )
+                    ( remainderBy bubbleMintDelay model.tick == 0 )
                 then
                     Random.generate MintBubble bubbleGenerator
                 else
@@ -104,16 +68,53 @@ update message model =
         StartAt mouseEvent ->
             ( burstBubbles model mouseEvent, Cmd.none )
 
-        MintBubble x -> ( { model
-                | bubbles = mintBubble model.bubbles x
+        MintBubble mintedBubble -> ( { model
+                | bubbles = mintedBubble :: model.bubbles
             }, Cmd.none )
-
-        --MoveAt _ -> ( model, Cmd.none )
-        --EndAt _ -> ( model, Cmd.none )
 
 
 subscriptions model =
     onAnimationFrameDelta Tick
+
+
+canvasWidth = 400
+canvasHeight = 512
+maxBubblesCount = 7
+bubbleMintDelay = 50
+bubbleMinSpeed = 2.5
+bubbleMaxSpeed = 5.0
+bubbleMinRadius = 20
+bubbleMaxRadius = 90
+bubbleMinX = 30
+bubbleMaxX = canvasWidth - bubbleMinX
+
+
+bubbleGenerator =
+    let
+        mintBubble from_x to_x radius speed =
+            { shape =
+                Circle2d.atPoint
+                ( Point2d.fromTuple Length.cssPixels ( from_x, -radius ) )
+                ( Length.cssPixels radius )
+            , velocity = 
+                Vector2d.withLength
+                ( Length.cssPixels speed )
+                ( Maybe.withDefault
+                    Direction2d.positiveY
+                    ( Direction2d.from
+                        ( Point2d.fromTuple
+                            Length.cssPixels
+                            ( from_x, -radius ) )
+                        ( Point2d.fromTuple
+                            Length.cssPixels
+                            ( to_x, canvasHeight ) ) ) ) }
+    in
+        Random.map4
+        mintBubble
+        ( Random.float bubbleMinX bubbleMaxX )            -- from_x
+        ( Random.float bubbleMinX bubbleMaxX )            -- to_x
+        ( Random.float bubbleMinRadius bubbleMaxRadius )  -- radius
+        ( Random.float bubbleMinSpeed bubbleMaxSpeed )    -- speed
 
 
 burstBubbles model mouseEvent =
@@ -148,18 +149,6 @@ moveBubbles bubbles =
                 Circle2d.translateBy bubble.velocity bubble.shape }
     in
         List.map moveBubble bubbles
-
-
-mintBubble bubbles x =
-    { shape =
-        Circle2d.atPoint
-        ( Point2d.fromTuple Length.cssPixels ( x, 0 ) )  -- center
-        ( Length.cssPixels 40 )  -- radius
-    , velocity =
-        Vector2d.withLength
-        ( Length.cssPixels 2.5 )  -- speed
-        Direction2d.positiveY
-    } :: bubbles
 
 
 clearCanvas =
